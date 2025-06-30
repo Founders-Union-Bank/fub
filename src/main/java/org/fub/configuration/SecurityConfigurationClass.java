511429ca7c4e4b658395e05a66788319b9d7f121
Sarankumar Vijayan <sarankumar.v@zirius.in>
2025-06-30 21:45:02 +0530

    Issues fix

--- src/main/java/org/fub/configuration/SecurityConfigurationClass.java
+++ src/main/java/org/fub/configuration/SecurityConfigurationClass.java
@@ -4,7 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,12 +25,10 @@ public class SecurityConfigurationClass {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request->
-                request.requestMatchers(
-                        "/v3/api-docs/**", "/swagger-ui/**","/test","/login").permitAll()
-                        .anyRequest().authenticated()
-                );
+                request.requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated());
         http.formLogin(AbstractHttpConfigurer::disable);
-        http.httpBasic(Customizer.withDefaults());
+        http.httpBasic(AbstractHttpConfigurer::disable);
         http.sessionManagement(smc->smc.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
